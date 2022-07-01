c3eb41f134410a70fc13b1bb2c6f8bf3a5903abc
Oksana <ashkeeper@mail.ru>
2022-07-01 11:08:40 +0300

    feat: Создать подзадачи.

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Subtask {
+import java.util.Objects;
+
+public class Subtask extends Task {
+    private Integer identifierEpic;
+    public Subtask( String name, Status status, Integer identifierEpic){
+        super(name,status);
+        this.identifierEpic = identifierEpic;
+    }
+
+    public Subtask(String name, Status status, Integer identifierEpic, int identifier) {
+        super(name, status, identifier);
+        this.identifierEpic = identifierEpic;
+    }
+
+    public void setIdentifierEpic(Integer identifierEpic) {
+        this.identifierEpic = identifierEpic;
+    }
+
+    public Integer getIdentifierEpic() {
+        return identifierEpic;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " Subtask{" +
+                "identifierEpic=" + identifierEpic +
+                '}';
+    }
 }
